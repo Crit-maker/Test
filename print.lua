3d218049f2d3c74e0e8721898ca0fe5888bae395
Crit-maker <69611506+Crit-maker@users.noreply.github.com>
2021-11-21 14:39:28 -0500

    Update print.lua

--- print.lua
+++ print.lua
@@ -1,36 +1,56 @@
-local head = game:GetService("Players").LocalPlayer.Character.Head
-_G.AutoSuper = true
-        _G.autotap = true
-    _G.autocombo = true
 
 
+    local check = 0
+    local head = game:GetService("Players").LocalPlayer.Character.Head
+    _G.autodungeon = true
+    _G.AutoSuper = true
+            _G.autotap = true
+        _G.autocombo = true
 
-
-    function tap()
-
-    game:GetService("ReplicatedStorage").Remotes.Events.Tap:FireServer()
-    end
-    while wait() do
-if _G.autotap == true then
-tap()
-end
-
-
-
-function combo()
-
-game:GetService("ReplicatedStorage").Remotes.Events.UseSuperTap:FireServer()
+local count = 0
+while _G.autodungeon == true do
+    wait()
+while count >= 4 do
+    wait()
+count = count + 1
+    for i, v in pairs(game:GetService("Workspace").Enemies:GetDescendants()) do
+game:GetService("ReplicatedStorage").Remotes.Events.AssignPet:FireServer(v)
+wait()
     end
 
-    if _G.autocombo == true then
-        combo()
+
+
+
+
+
+        function tap()
+
+        game:GetService("ReplicatedStorage").Remotes.Events.Tap:FireServer()
+        end
+        while wait() do
+    if _G.autotap == true then
+    tap()
     end
-end
 
-function Super()
-game:GetService("ReplicatedStorage").Remotes.Events.UseSuperTap:FireServer()
-end
 
-if _G.AutoSuper == true then
-Super()
+
+    function combo()
+
+
+    game:GetService("ReplicatedStorage").Remotes.Events.UpdateCombo:FireServer()
+        end
+
+        if _G.autocombo == true then
+            combo()
+        end
+    end
+
+    function Super()
+    game:GetService("ReplicatedStorage").Remotes.Events.UseSuperTap:FireServer()
+    end
+
+    if _G.AutoSuper == true then
+    Super()
+    end
+    end
 end
