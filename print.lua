2d97567db0ab8a1bcc564c60afe31db6e169bdcb
Crit-maker <69611506+Crit-maker@users.noreply.github.com>
2021-11-21 00:38:52 -0500

    Update print.lua

--- print.lua
+++ print.lua
@@ -1,1 +1,36 @@
-print("hello World")
+local head = game:GetService("Players").LocalPlayer.Character.Head
+_G.AutoSuper = true
+        _G.autotap = true
+    _G.autocombo = true
+
+
+
+
+    function tap()
+
+    game:GetService("ReplicatedStorage").Remotes.Events.Tap:FireServer()
+    end
+    while wait() do
+if _G.autotap == true then
+tap()
+end
+
+
+
+function combo()
+
+game:GetService("ReplicatedStorage").Remotes.Events.UseSuperTap:FireServer()
+    end
+
+    if _G.autocombo == true then
+        combo()
+    end
+end
+
+function Super()
+game:GetService("ReplicatedStorage").Remotes.Events.UseSuperTap:FireServer()
+end
+
+if _G.AutoSuper == true then
+Super()
+end
